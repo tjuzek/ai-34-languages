2ed2067b602539138d0c49e44231fb2d90b3dc56
Thomas Stephan Juzek <116889702+tjuzek@users.noreply.github.com>
2026-03-17 01:19:30 -0400

    Add files via upload

---
+++ src/aiwl/__init__.py
@@ -0,0 +1,8 @@
+"""AIWL: AI & the World’s Languages.
+
+This package will eventually host shared utilities for:
+- corpus preprocessing
+- tagging and lemmatisation backends
+- LAS/TPS computation
+- cross-linguistic aggregation and plotting
+"""
